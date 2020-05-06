4b2c05e143d91304b38ca01cc86b70e06202a408
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2020-05-06 13:09:27 -0400

    Went through the class exercise

--- src/main/java/com/azuresolute/Main.java
+++ src/main/java/com/azuresolute/Main.java
@@ -1,7 +1,35 @@
 package com.azuresolute;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Happy");
+        try {
+//            writeFile();
+            readFile();
+        } catch(IOException ex) {
+            System.out.println("what went wrong?");
+        }
+
+    }
+
+    public static void writeFile()  throws IOException {
+        PrintWriter out = new PrintWriter(new FileWriter("OutFile.txt"));
+        out.println("this is a line in my file...");
+        out.println("a second line in my file...");
+        out.println("a third line in my file...");
+        out.flush();
+        out.close();
+    }
+
+    public static void readFile() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("outFile.txt"));
+        Scanner scanner = new Scanner(reader);
+
+        while(scanner.hasNextLine()) {
+            String currentLine = scanner.nextLine();
+            System.out.println(currentLine);
+        }
     }
 }

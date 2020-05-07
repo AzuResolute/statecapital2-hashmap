e4dc2f600a7231fd30e1ad558f9ee92bca829084
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2020-05-07 18:48:30 -0400

    Fixed userIO

--- src/main/java/com/userio/UserIOImpl.java
+++ src/main/java/com/userio/UserIOImpl.java
@@ -3,7 +3,6 @@ package com.userio;
 import java.util.Scanner;
 
 public class UserIOImpl implements UserIO {
-    private static Scanner ui = new Scanner(System.in);
     @Override
     public void print(String message) {
         System.out.println(message);
@@ -11,150 +10,85 @@ public class UserIOImpl implements UserIO {
 
     @Override
     public String readString(String prompt) {
-        String answer = "";
-        try {
-            System.out.println(prompt);
-            answer = ui.nextLine();
-
-        } catch (Exception ex) {
-            System.out.println("Something wrong");
-        }
-        return answer;
+        Scanner scan = new Scanner(System.in);
+        System.out.println(prompt);
+        String out = scan.nextLine();
+        return out;
     }
 
     @Override
     public int readInt(String prompt) {
-        int result = 0;
-
-        System.out.print(prompt);
-        try {
-            result = ui.nextInt();
-        } catch (Exception ex) {
-            System.out.println("Invalid Input");
-        }
-        return result;
+        Scanner scan = new Scanner(System.in);
+        System.out.println(prompt);
+        int out = scan.nextInt();
+        return out;
     }
 
     @Override
     public int readInt(String prompt, int min, int max) {
-        int result = min - 1;
-        boolean valid = false;
-
-        System.out.print(prompt);
-        try {
-            do {
-                result = ui.nextInt();
-                if(result >= min && result <= max) {
-                    valid = true;
-                } else {
-                    System.out.println("Invalid Input. Please try again.");
-                }
-            } while(!valid);
-        } catch (Exception ex) {
-            System.out.println("Invalid Input");
-        }
-        return result;
+        Scanner scan = new Scanner(System.in);
+        int out = 0;
+        do {
+            System.out.println(prompt);
+            out = scan.nextInt();
+        } while(out < min || out > max);
+        return out;
     }
 
     @Override
     public double readDouble(String prompt) {
-        double result = 0;
-
-        System.out.print(prompt);
-        try {
-            result = ui.nextDouble();
-        } catch (Exception ex) {
-            System.out.println("Invalid Input");
-        }
-        return result;
+        Scanner scan = new Scanner(System.in);
+        System.out.println(prompt);
+        double out = scan.nextDouble();
+        return out;
     }
 
     @Override
     public double readDouble(String prompt, double min, double max) {
-        double result = min - 1;
-        boolean valid = false;
-
-        System.out.print(prompt);
-        try {
-            do {
-                result = ui.nextDouble();
-                if(result >= min && result <= max) {
-                    valid = true;
-                } else {
-                    System.out.println("Invalid Input. Please try again.");
-                }
-            } while(!valid);
-        } catch (Exception ex) {
-            System.out.println("Invalid Input");
-        }
-        return result;
+        Scanner scan = new Scanner(System.in);
+        double out = 0;
+        do {
+            System.out.println(prompt);
+            out = scan.nextDouble();
+        } while(out < min || out > max);
+        return out;
     }
 
     @Override
     public float readFloat(String prompt) {
-        float result = 0;
-
-        System.out.print(prompt);
-        try {
-            result = ui.nextFloat();
-        } catch (Exception ex) {
-            System.out.println("Invalid Input");
-        }
-        return result;
+        Scanner scan = new Scanner(System.in);
+        System.out.println(prompt);
+        float out = scan.nextFloat();
+        return out;
     }
 
     @Override
     public float readFloat(String prompt, float min, float max) {
-        float result = min - 1f;
-        boolean valid = false;
-
-        System.out.print(prompt);
-        try {
-            do {
-                result = ui.nextFloat();
-                if(result >= min && result <= max) {
-                    valid = true;
-                } else {
-                    System.out.println("Invalid Input. Please try again.");
-                }
-            } while(!valid);
-        } catch (Exception ex) {
-            System.out.println("Invalid Input");
-        }
-        return result;
+        Scanner scan = new Scanner(System.in);
+        float out = 0;
+        do {
+            System.out.println(prompt);
+            out = scan.nextFloat();
+        } while(out < min || out > max);
+        return out;
     }
 
     @Override
     public long readLong(String prompt) {
-        long result = 0;
-
-        System.out.print(prompt);
-        try {
-            result = ui.nextLong();
-        } catch (Exception ex) {
-            System.out.println("Invalid Input");
-        }
-        return result;
+        Scanner scan = new Scanner(System.in);
+        System.out.println(prompt);
+        long out = scan.nextLong();
+        return out;
     }
 
     @Override
     public long readLong(String prompt, long min, long max) {
-        long result = min - 1L;
-        boolean valid = false;
-
-        System.out.print(prompt);
-        try {
-            do {
-                result = ui.nextLong();
-                if(result >= min && result <= max) {
-                    valid = true;
-                } else {
-                    System.out.println("Invalid Input. Please try again.");
-                }
-            } while(!valid);
-        } catch (Exception ex) {
-            System.out.println("Invalid Input");
-        }
-        return result;
+        Scanner scan = new Scanner(System.in);
+        long out = 0;
+        do {
+            System.out.println(prompt);
+            out = scan.nextLong();
+        } while(out < min || out > max);
+        return out;
     }
 }
